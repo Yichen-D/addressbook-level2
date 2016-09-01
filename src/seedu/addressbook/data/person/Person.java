1fc2dc6be66ed3ed9562b11b6392713e0fd3e1f0
DESKTOP-E8PT9RL\Yichen <victoriaduanyc@gmail.com>
2016-09-01 19:14:26 +0900

    Add class-level variable nextSequenceNumber

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -14,8 +14,10 @@ public class Person implements ReadOnlyPerson {
     private Phone phone;
     private Email email;
     private Address address;
+    private int sequenceNumber;
 
     private final UniqueTagList tags;
+    private static int nextSequenceNumber = 0;
     /**
      * Assumption: Every field must be present and not null.
      */
@@ -24,7 +26,8 @@ public class Person implements ReadOnlyPerson {
         this.phone = phone;
         this.email = email;
         this.address = address;
-        this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        this.tags = new UniqueTagList(tags); 
+        this.sequenceNumber = ++nextSequenceNumber;// protect internal tags from changes in the arg list
     }
 
     /**
